a4f4c32fe36e692a8c26a5e62ee7d0191eab22db
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2023-01-02 11:35:08 -0300

    feature: Group card component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { GroupCard } from '../../components/GroupCard';
 import { Header } from '../../components/Header';
 import { HightLight } from '../../components/HightLight';
+
 import * as style from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera de SP', 'Galera do trabalho']);
+
   return (
     <style.Container>
       <Header  />
@@ -10,6 +16,12 @@ export function Groups() {
       <HightLight
         title='Turmas'
         subtitle='Jogue com a sua turma'
+      />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item}/>}
       />
     </style.Container>
   );
