096068bdb8b6b1ed9015b8e792108da9fe8c5f79
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2023-01-02 10:37:26 -0300

    feature: Load indicator component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { ThemeProvider } from 'styled-components'
 import { StatusBar } from 'expo-status-bar';
 import { Groups } from './src/screens/Groups';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 import theme from './src/theme';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
-      <StatusBar style="auto" />
+      {fontsLoaded ? <Groups /> : <Loading /> }
+      <StatusBar style='light' backgroundColor='transparent' translucent />
     </ThemeProvider>
   );
 }
