618f9ef088f9cfd54399ea89c9e9bf5d3ff25a4f
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2023-01-02 10:55:53 -0300

    feature: Header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from '../../components/Header';
 import * as style from './styles';
 
 export function Groups() {
   return (
     <style.Container>
-
+      <Header  />
     </style.Container>
   );
 }
